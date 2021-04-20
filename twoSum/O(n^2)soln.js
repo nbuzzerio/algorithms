1ca149df67267a5859dee0613f08e6ca6da55a88
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-04-20 17:04:18 -0400

    O(n^2) solution to TwoSum

---
+++ twoSum/O(n^2)soln.js
@@ -0,0 +1,9 @@
+var twoSum = function (nums, target) {
+  for (let a = 0; a < nums.length; a++) {
+    for (let b = a + 1; b < nums.length; b++) {
+      if (nums[a] + nums[b] === target) {
+        return [a, b];
+      }
+    }
+  }
+};
