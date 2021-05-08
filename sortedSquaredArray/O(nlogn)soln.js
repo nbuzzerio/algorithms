3abfbb1114c336d8febc06c204178e93c204470b
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-08 13:23:22 -0400

    O(nlogn) solution to sortedSquaredArray

---
+++ sortedSquaredArray/O(nlogn)soln.js
@@ -0,0 +1,12 @@
+/*
+    Given a sorted array of integers return a sorted array of those integers squared.
+*/
+
+function sortedSquaredArray(array) {
+  const squared = array.map((int) => int ** 2);
+  if (array[0] < 0)
+    return squared.sort((a, b) => {
+      if (a < b) return -1;
+    });
+  else return squared;
+}
