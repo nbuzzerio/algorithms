9991346dd9cfe6be0c8ec6238d7ca9b397ae5df8
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-10 15:16:43 -0400

    O(nlogn) solution to smallestNonConstructibleChange

---
+++ smallestNonConstructibleChange/O(nlogn)soln.js
@@ -0,0 +1,27 @@
+//Write a function that returns the smallest non-constructible amount of change given the coin values in the array.
+
+function nonConstructibleChange(coins) {
+  coins = coins.sort((a, b) => a - b);
+
+  let potentialNCC = 1;
+  let currentTotal = 0;
+  let incr = true;
+
+  while (potentialNCC > currentTotal && incr) {
+    for (let i = coins.length; i >= 0; i--) {
+      if (currentTotal + coins[i] <= potentialNCC) {
+        currentTotal += coins[i];
+      }
+    }
+
+    if (currentTotal === potentialNCC) {
+      potentialNCC++;
+      currentTotal = 0;
+    } else {
+      incr = false;
+    }
+  }
+
+  return potentialNCC;
+}
+//O(nlogn) time where n is the size of the coins array | O(1) space
